ceeba5e58d24175643bfe6f2ea0e615a248e5422
zoufuliang <2398145311@qq.com>
2017-11-21 15:21:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+ private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/entry/GoodsBean.java
@@ -0,0 +1,9 @@
+package com.example.administrator.myapplication.entry;
+
+/**
+ * Created by Administrator on 2017/11/20.
+ */
+
+public class GoodsBean {
+
+}
